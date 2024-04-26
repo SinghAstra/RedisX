d1af6d1a08f5e1601f1ce026702abb55ecc87fbc
Astra <abhaypratapsinghwd@gmail.com>
2024-04-26 14:28:37 +0530

    Added particle effect animation

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -1,11 +1,103 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
 
 const NotFound = () => {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
   return (
-    <div className="h-screen bg-slate-400 flex items-center flex-col justify-center gap-40">
-      {/* <h1 className="text-3xl font-mono text-black tracking-wide">Not Found</h1> */}
-      <div className="inline-block bg-yellow-400">inline block</div>
-      <div className="block bg-green-400">block</div>
+    <div>
+      {init && (
+        <Particles
+          id="tsparticles"
+          particlesLoaded={particlesLoaded}
+          options={{
+            background: {
+              color: {
+                value: "black",
+              },
+            },
+            fpsLimit: 120,
+            interactivity: {
+              events: {
+                onClick: {
+                  enable: true,
+                  mode: "push",
+                },
+                onHover: {
+                  enable: true,
+                  mode: "repulse",
+                },
+                resize: true,
+              },
+              modes: {
+                push: {
+                  quantity: 4,
+                },
+                repulse: {
+                  distance: 200,
+                  duration: 0.4,
+                },
+              },
+            },
+            particles: {
+              color: {
+                value: "",
+              },
+              links: {
+                color: "#ffffff",
+                distance: 150,
+                enable: true,
+                opacity: 0.5,
+                width: 1,
+              },
+              move: {
+                direction: "none",
+                enable: true,
+                outModes: {
+                  default: "bounce",
+                },
+                random: false,
+                speed: 6,
+                straight: false,
+              },
+              number: {
+                density: {
+                  enable: true,
+                  area: 800,
+                },
+                value: 80,
+              },
+              opacity: {
+                value: 0.5,
+              },
+              shape: {
+                type: "circle",
+              },
+              size: {
+                value: { min: 1, max: 5 },
+              },
+            },
+            detectRetina: true,
+          }}
+        ></Particles>
+      )}
+      <div className="h-screen flex items-center flex-col justify-center gap-40 z-50">
+        <h1 className="text-3xl font-mono text-black tracking-wide">
+          Not Found
+        </h1>
+      </div>
     </div>
   );
 };
--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -1,62 +1,157 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Register.css";
 import { Link } from "react-router-dom";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
 
 const Register = () => {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
   return (
-    <div className="register-container h-screen flex">
-      <div className="form-data flex-1 p-10 flex items-start justify-between flex-col">
-        <div>
-          <Link className="hover:text-cyan-400 text-2xl" to="/">
-            WebAuth
-          </Link>
+    <div>
+      {init && (
+        <Particles
+          id="tsparticles"
+          particlesLoaded={particlesLoaded}
+          options={{
+            background: {
+              color: {
+                value: "black",
+              },
+            },
+            fpsLimit: 120,
+            interactivity: {
+              events: {
+                onClick: {
+                  enable: true,
+                  mode: "push",
+                },
+                onHover: {
+                  enable: true,
+                  mode: "repulse",
+                },
+                resize: true,
+              },
+              modes: {
+                push: {
+                  quantity: 4,
+                },
+                repulse: {
+                  distance: 200,
+                  duration: 0.4,
+                },
+              },
+            },
+            particles: {
+              color: {
+                value: "",
+              },
+              links: {
+                color: "#ffffff",
+                distance: 150,
+                enable: true,
+                opacity: 0.5,
+                width: 1,
+              },
+              move: {
+                direction: "none",
+                enable: true,
+                outModes: {
+                  default: "bounce",
+                },
+                random: false,
+                speed: 6,
+                straight: false,
+              },
+              number: {
+                density: {
+                  enable: true,
+                  area: 800,
+                },
+                value: 80,
+              },
+              opacity: {
+                value: 0.5,
+              },
+              shape: {
+                type: "circle",
+              },
+              size: {
+                value: { min: 1, max: 5 },
+              },
+            },
+            detectRetina: true,
+          }}
+        />
+      )}
+      <div className="register-container h-screen flex">
+        <div className="form-data flex-1 p-10 flex items-start justify-between flex-col">
+          <div>
+            <Link className="hover:text-cyan-400 text-2xl" to="/">
+              WebAuth
+            </Link>
+          </div>
+          <div className="max-w-md w-full">
+            <h1 className="uppercase text-neutral-400 font-bold mb-1">
+              Start for free
+            </h1>
+            <h1 className="text-white text-2xl font-medium mb-4">
+              Create New Account
+            </h1>
+            <h3 className="hover:cursor-pointer hover:underline mb-6">
+              Already Have an Account?
+              <span className="text-cyan-400 "> Sign In </span>
+            </h3>
+            <form className="flex flex-col gap-3">
+              <div className="relative input-container">
+                <input type="text" className="data-input" placeholder=" " />
+                <label className="data-label text-slate-400">Username</label>
+              </div>
+              <div className="relative input-container">
+                <input type="email" className="data-input" placeholder=" " />
+                <label className="data-label text-slate-400">Email</label>
+              </div>
+              <div className="relative input-container">
+                <input type="password" className="data-input" placeholder=" " />
+                <label className="data-label text-slate-400">Password</label>
+              </div>
+              <button className="bg-cyan-400 group flex items-center gap-2.5 rounded-full w-fit text-black font-medium text-lg px-4 py-1 transition-[shadow,filter]  hover:bg-cyan-500 mt-4 ">
+                <span>Create account</span>
+                <svg
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  focusable="false"
+                  shapeRendering="geometricPrecision"
+                  viewBox="0 0 20 20"
+                  className="w-6 h-6 transition-transform duration-250 group-hover:translate-x-1"
+                >
+                  <path
+                    d="m12 4 6 6-6 6M2 10h16"
+                    stroke="currentColor"
+                    vectorEffect="non-scaling-stroke"
+                  ></path>
+                </svg>
+              </button>
+            </form>
+          </div>
         </div>
-        <div className="max-w-md w-full">
-          <h1 className="uppercase text-neutral-400 font-bold mb-1">
-            Start for free
-          </h1>
-          <h1 className="text-white text-2xl font-medium mb-4">
-            Create New Account
-          </h1>
-          <h3 className="hover:cursor-pointer hover:underline mb-6">
-            Already Have an Account?
-            <span className="text-cyan-400 "> Sign In </span>
-          </h3>
-          <form className="flex flex-col gap-3">
-            <div className="relative input-container">
-              <input type="text" className="data-input" placeholder=" " />
-              <label className="data-label text-slate-400">Username</label>
-            </div>
-            <div className="relative input-container">
-              <input type="email" className="data-input" placeholder=" " />
-              <label className="data-label text-slate-400">Email</label>
-            </div>
-            <div className="relative input-container">
-              <input type="password" className="data-input" placeholder=" " />
-              <label className="data-label text-slate-400">Password</label>
-            </div>
-            <button className="bg-cyan-400 group flex items-center gap-2.5 rounded-full w-fit text-black font-medium text-lg px-4 py-1 transition-[shadow,filter]  hover:bg-cyan-500 mt-4 ">
-              <span>Create account</span>
-              <svg
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                focusable="false"
-                shapeRendering="geometricPrecision"
-                viewBox="0 0 20 20"
-                className="w-6 h-6 transition-transform duration-250 group-hover:translate-x-1"
-              >
-                <path
-                  d="m12 4 6 6-6 6M2 10h16"
-                  stroke="currentColor"
-                  vectorEffect="non-scaling-stroke"
-                ></path>
-              </svg>
-            </button>
-          </form>
+        <div className="flex-1 anime-container ">
+          <img
+            src={"./img/anime.png"}
+            alt="anime"
+            className="anime-image z-20"
+          />
         </div>
-      </div>
-      <div className="flex-1 anime-container">
-        <img src={"./img/anime.png"} alt="anime" className="anime-image" />
       </div>
     </div>
   );
