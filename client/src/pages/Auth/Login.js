3e4787ea8edc53023e86d04d75c6ee1dd997a3a0
Astra <abhaypratapsinghwd@gmail.com>
2024-04-26 17:41:57 +0530

    Added formik and yup validation to register form

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -90,56 +90,50 @@ const Login = () => {
         />
       )}
       {init && (
-        <div className="register-container h-screen flex bg-[#212122]">
-          <div className="w-2/5 p-10 flex items-start justify-between flex-col ">
-            <div>
-              <Link className="hover:text-cyan-400 text-2xl" to="/">
-                WebAuth
-              </Link>
-            </div>
-            <div className="max-w-md w-full form-data ">
-              <h1 className="uppercase text-neutral-400 font-bold mb-1">
-                Welcome Back
-              </h1>
-              <h1 className="text-white text-2xl font-medium mb-4">Sign In</h1>
-              <Link
-                className="hover:cursor-pointer hover:underline"
-                to={"/register"}
-              >
-                New User ?<span className="text-cyan-400 "> Register </span>
-              </Link>
-              <form className="flex flex-col gap-3 mt-6">
-                <div className="relative input-container">
-                  <input type="email" className="data-input" placeholder=" " />
-                  <label className="data-label text-slate-400">Email</label>
-                </div>
-                <div className="relative input-container">
-                  <input
-                    type="password"
-                    className="data-input"
-                    placeholder=" "
-                  />
-                  <label className="data-label text-slate-400">Password</label>
-                </div>
-                <button className="bg-cyan-400 group flex items-center gap-2.5 rounded-full w-fit text-black font-medium text-lg px-4 py-1 transition-[shadow,filter]  hover:bg-cyan-500 mt-4 ">
-                  <span>Access account</span>
-                  <svg
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    focusable="false"
-                    shapeRendering="geometricPrecision"
-                    viewBox="0 0 20 20"
-                    className="w-6 h-6 transition-transform duration-250 group-hover:translate-x-1"
-                  >
-                    <path
-                      d="m12 4 6 6-6 6M2 10h16"
-                      stroke="currentColor"
-                      vectorEffect="non-scaling-stroke"
-                    ></path>
-                  </svg>
-                </button>
-              </form>
-            </div>
+        <div className="h-screen bg-[#212122] flex flex-col items-start justify-between">
+          <div className="p-6">
+            <Link className="hover:text-cyan-400 text-2xl" to="/">
+              WebAuth
+            </Link>
+          </div>
+          <div className="max-w-md w-full form-data p-6">
+            <h1 className="uppercase text-neutral-400 font-bold mb-1">
+              Welcome Back
+            </h1>
+            <h1 className="text-white text-2xl font-medium mb-4">Sign In</h1>
+            <Link
+              className="hover:cursor-pointer hover:underline"
+              to={"/register"}
+            >
+              New User ?<span className="text-cyan-400 "> Register </span>
+            </Link>
+            <form className="flex flex-col gap-3 mt-6">
+              <div className="relative input-container">
+                <input type="email" className="data-input" placeholder=" " />
+                <label className="data-label text-slate-400">Email</label>
+              </div>
+              <div className="relative input-container">
+                <input type="password" className="data-input" placeholder=" " />
+                <label className="data-label text-slate-400">Password</label>
+              </div>
+              <button className="bg-cyan-400 group flex items-center gap-2.5 rounded-full w-fit text-black font-medium text-lg px-4 py-1 transition-[shadow,filter]  hover:bg-cyan-500 mt-4 ">
+                <span>Access account</span>
+                <svg
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  focusable="false"
+                  shapeRendering="geometricPrecision"
+                  viewBox="0 0 20 20"
+                  className="w-6 h-6 transition-transform duration-250 group-hover:translate-x-1"
+                >
+                  <path
+                    d="m12 4 6 6-6 6M2 10h16"
+                    stroke="currentColor"
+                    vectorEffect="non-scaling-stroke"
+                  ></path>
+                </svg>
+              </button>
+            </form>
           </div>
         </div>
       )}
--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -1,10 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./Auth.css";
 import { Link } from "react-router-dom";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { loadSlim } from "@tsparticles/slim";
+import * as Yup from "yup";
+import { Formik } from "formik";
 
 const Register = () => {
+  const initialValues = {
+    username: "",
+    email: "",
+    password: "",
+  };
+
+  const RegisterSchema = Yup.object().shape({
+    username: Yup.string().required("Username is required"),
+    email: Yup.string()
+      .required("Email is required")
+      .email("Invalid email format"),
+    password: Yup.string()
+      .min(8, "Password must be at least 8 characters long")
+      .matches(/[a-z]/, "Password requires a lowercase letter")
+      .matches(/[A-Z]/, "Password requires an uppercase letter")
+      .matches(/[0-9]/, "Password requires a number")
+      .matches(/[^\w\s]/, "Password requires a special character")
+      .required("Password is required"),
+  });
+
   const [init, setInit] = useState(false);
 
   useEffect(() => {
@@ -14,6 +36,11 @@ const Register = () => {
       setInit(true);
     });
   }, []);
+
+  const onSubmit = (values, { setSubmitting }) => {
+    console.log("Form submitted:", values);
+    setSubmitting(false); // Reset form submission state
+  };
 
   return (
     <div>
@@ -92,63 +119,123 @@ const Register = () => {
         />
       )}
       {init && (
-        <div className="register-container h-screen flex bg-[#212122]">
-          <div className="w-2/5 p-10 flex items-start justify-between flex-col ">
-            <div>
-              <Link className="hover:text-cyan-400 text-2xl" to="/">
-                WebAuth
-              </Link>
-            </div>
-            <div className="max-w-md w-full form-data ">
-              <h1 className="uppercase text-neutral-400 font-bold mb-1">
-                Start for free
-              </h1>
-              <h1 className="text-white text-2xl font-medium mb-4">
-                Create New Account
-              </h1>
-              <Link
-                className="hover:cursor-pointer hover:underline "
-                to={"/log-in"}
-              >
-                Already Have an Account?
-                <span className="text-cyan-400 "> Sign In </span>
-              </Link>
-              <form className="flex flex-col gap-3 mt-6">
-                <div className="relative input-container">
-                  <input type="text" className="data-input" placeholder=" " />
-                  <label className="data-label text-slate-400">Username</label>
-                </div>
-                <div className="relative input-container">
-                  <input type="email" className="data-input" placeholder=" " />
-                  <label className="data-label text-slate-400">Email</label>
-                </div>
-                <div className="relative input-container">
-                  <input
-                    type="password"
-                    className="data-input"
-                    placeholder=" "
-                  />
-                  <label className="data-label text-slate-400">Password</label>
-                </div>
-                <button className="bg-cyan-400 group flex items-center gap-2.5 rounded-full w-fit text-black font-medium text-lg px-4 py-1 transition-[shadow,filter]  hover:bg-cyan-500 mt-4 ">
-                  <span>Create account</span>
-                  <svg
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    focusable="false"
-                    shapeRendering="geometricPrecision"
-                    viewBox="0 0 20 20"
-                    className="w-6 h-6 transition-transform duration-250 group-hover:translate-x-1"
+        <div className="h-screen bg-[#212122] flex flex-col items-start justify-between">
+          <div className="p-6">
+            <Link className="hover:text-cyan-400 text-2xl" to="/">
+              WebAuth
+            </Link>
+          </div>
+          <div className="max-w-md w-full form-data p-6">
+            <h1 className="uppercase text-neutral-400 font-bold mb-1">
+              Start for free
+            </h1>
+            <h1 className="text-white text-2xl font-medium mb-4">
+              Create New Account
+            </h1>
+            <Link
+              className="hover:cursor-pointer hover:underline "
+              to={"/log-in"}
+            >
+              Already Have an Account?
+              <span className="text-cyan-400 "> Sign In </span>
+            </Link>
+            <Formik
+              initialValues={{ username: "", email: "", password: "" }}
+              validationSchema={RegisterSchema}
+              onSubmit={onSubmit}
+            >
+              {({
+                values,
+                handleChange,
+                handleSubmit,
+                errors,
+                touched,
+                handleBlur,
+              }) => {
+                return (
+                  <form
+                    className="flex flex-col gap-3 mt-6"
+                    onSubmit={handleSubmit}
                   >
-                    <path
-                      d="m12 4 6 6-6 6M2 10h16"
-                      stroke="currentColor"
-                      vectorEffect="non-scaling-stroke"
-                    ></path>
-                  </svg>
-                </button>
-              </form>
-            </div>
+                    <div className="relative input-container">
+                      <input
+                        type="text"
+                        className="data-input"
+                        placeholder=" "
+                        name="username"
+                        value={values.username}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                      />
+                      <label className="data-label text-slate-400">
+                        Username
+                      </label>
+                      {errors.username && touched.username ? (
+                        <div className="text-red-500 text-xs px-2">
+                          {errors.username}
+                        </div>
+                      ) : null}
+                    </div>
+                    <div className="relative input-container">
+                      <input
+                        type="email"
+                        className="data-input"
+                        placeholder=" "
+                        name="email"
+                        value={values.email}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                      />
+                      <label className="data-label text-slate-400">Email</label>
+                      {errors.email && touched.email ? (
+                        <div className="text-red-500 text-xs px-2">
+                          {errors.email}
+                        </div>
+                      ) : null}
+                    </div>
+                    <div className="relative input-container">
+                      <input
+                        type="password"
+                        className="data-input"
+                        placeholder=" "
+                        name="password"
+                        value={values.password}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                      />
+                      <label className="data-label text-slate-400">
+                        Password
+                      </label>
+                      {errors.password && touched.password ? (
+                        <div className="text-red-500 text-xs px-2">
+                          {errors.password}
+                        </div>
+                      ) : null}
+                    </div>
+                    <button
+                      className="bg-cyan-400 group flex items-center gap-2.5 rounded-full w-fit text-black font-medium text-lg px-4 py-1 transition-[shadow,filter]  hover:bg-cyan-500 mt-4 "
+                      type="submit"
+                    >
+                      <span>Create account</span>
+                      <svg
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        focusable="false"
+                        shapeRendering="geometricPrecision"
+                        viewBox="0 0 20 20"
+                        className="w-6 h-6 transition-transform duration-250 group-hover:translate-x-1"
+                      >
+                        <path
+                          d="m12 4 6 6-6 6M2 10h16"
+                          stroke="currentColor"
+                          vectorEffect="non-scaling-stroke"
+                        ></path>
+                      </svg>
+                    </button>
+                  </form>
+                );
+              }}
+            </Formik>
           </div>
         </div>
       )}
