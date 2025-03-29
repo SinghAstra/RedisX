1598dce25020f26eb523287bab303b1e7e3d6ffc
Astra <abhaypratapsinghwd@gmail.com>
2025-03-29 11:47:55 +0530

    Deployment Bug Fix

--- config/nav.ts
+++ config/nav.ts
@@ -31,67 +31,6 @@ export const navLinks: NavLinks = {
           label: "Code",
           items: [],
         },
-        {
-          title: "components.json",
-          href: "/docs/components-json",
-          items: [],
-        },
-        {
-          title: "Theming",
-          href: "/docs/theming",
-          items: [],
-        },
-        {
-          title: "Dark mode",
-          href: "/docs/dark-mode",
-          items: [],
-        },
-        {
-          title: "CLI",
-          href: "/docs/cli",
-          items: [],
-        },
-        {
-          title: "Monorepo",
-          href: "/docs/monorepo",
-          items: [],
-        },
-        {
-          title: "Tailwind v4",
-          href: "/docs/tailwind-v4",
-          items: [],
-          label: "New",
-        },
-        {
-          title: "Next.js 15 + React 19",
-          href: "/docs/react-19",
-          items: [],
-        },
-        {
-          title: "Typography",
-          href: "/docs/components/typography",
-          items: [],
-        },
-        {
-          title: "Open in v0",
-          href: "/docs/v0",
-          items: [],
-        },
-        {
-          title: "Blocks",
-          href: "/docs/blocks",
-          items: [],
-        },
-        {
-          title: "Figma",
-          href: "/docs/figma",
-          items: [],
-        },
-        {
-          title: "Changelog",
-          href: "/docs/changelog",
-          items: [],
-        },
       ],
     },
   ],
